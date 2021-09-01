6a9d7b0bd972b5e79ee30dce5b1c92c131c7ba46
leandrodcs <eng.leandrodcs@gmail.com>
2021-09-01 11:05:56 -0300

    Creating header component

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Header';
+
+export default function App() {
+  return (
+
+    <>
+    <Header />
+    </>
+  );
+}
+
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export default function Headers() {
+    return (
+        <>
+        <header>CINEFLEX</header>
+        </>
+    )
+
+}
